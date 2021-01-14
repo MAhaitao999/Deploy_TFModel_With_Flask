0bf08191d6fbe2274567606a0e0f4f55ef667f69
Henry Ma <haitao.ma@transwarp.io>
2021-01-14 14:51:29 +0800

    rewrite main.py

--- loadtest/ApacheBench/load_test.sh
+++ loadtest/ApacheBench/load_test.sh
@@ -1,1 +1,1 @@
-ab -n 100 -c 24 -T 'application/json' -H "accept: application/json" -p dog_req.json http://127.0.0.1:5000/stream_predict
+ab -n 100 -c 24 -T 'application/json' -H "accept: application/json" -p dog_req.json http://127.0.0.1:5000/predict
--- main.py
+++ main.py
@@ -5,6 +5,8 @@ Created by HenryMa on 2021/1/12
 __author__ = 'HenryMa'
 
 
+import io
+import base64
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 from builtins import *
@@ -13,6 +15,21 @@ import cv2
 import numpy as np
 import tensorflow as tf
 from flask import Flask
+from flask import request
+from flask import jsonify
+from PIL import Image
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import img_to_array
+
+
+def preprocess_image(image, target_size):
+    if image.mode != 'RGB':
+        image = image.convert('RGB')
+    image = image.resize(target_size)
+    image = img_to_array(image)
+    image = np.expand_dims(image, axis=0)
+
+    return image
 
 
 class BaseAPP(object):
@@ -33,22 +50,22 @@ class BaseAPP(object):
         """
         Load trained model.
         """
-        print("Loading model...")
+        print('Loading model...')
 
         with tf.io.gfile.GFile(model_filepath, 'rb') as f:  # tf.gfile.GFile
             graph_def = tf.compat.v1.GraphDef()  # tf.GraphDef
             graph_def.ParseFromString(f.read())
 
         with self.graph.as_default():
-            tf.import_graph_def(graph_def, name="")
+            tf.import_graph_def(graph_def, name='')
         self.graph.finalize()
 
-        print("Model loading complete!")
+        print('Model loading complete!')
         self.sess = tf.compat.v1.Session(graph=self.graph)  # tf.Session
 
     def forward(self, feet_data):
         if len(feet_data) != len(self.input_names):
-            raise Exception("输入个数和喂给的数据个数必须一致")
+            raise Exception('The number of feed_data must satisfy with the length of list input_names!')
         input_ = {}
         for i in range(len(self.input_names)):
             input_[self.graph.get_tensor_by_name(self.input_names[i])] = feet_data[i]
@@ -62,25 +79,25 @@ class BaseAPP(object):
         return results
 
 
-model_name = "frozen_darknet_yolov3_model.pb"
-test_image = "dog.jpg"
+model_name = 'frozen_darknet_yolov3_model.pb'
+test_image = 'dog.jpg'
 face_mask = BaseAPP(model_name, input_names=['inputs:0'],
                     output_names=['output_boxes:0'])
-
-img = cv2.imread(test_image)
-img = cv2.resize(img, (416, 416))
-img = np.expand_dims(img, axis=0)
 
 app = Flask(__name__)
 
 
-@app.route('/predict')
+@app.route('/predict', methods=['POST'])
 def predict():
-    print("=================", os.getpid())
-    print("Hello world")
-    face_mask.forward([img])
-    return "hello world"
+    message = request.get_json(force=True)
+    encoded = message['image']
+    decoded = base64.b64decode(encoded)
+    image = Image.open(io.BytesIO(decoded))
+    processed_image = preprocess_image(image, target_size=(416, 416))
+    outputs = face_mask.forward([processed_image])
+    
+    return jsonify({'result': outputs[0][0].tolist()})
 
 
-if __name__ == "__main__":
-    app.run(debug=True, threaded=True, processes=1)
+if __name__ == '__main__':
+    app.run(debug=False, threaded=True, processes=1)
