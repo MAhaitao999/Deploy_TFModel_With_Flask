8d40755ef78ef12444fd9ed5173b760934160a62
Henry Ma <haitao.ma@transwarp.io>
2021-01-27 18:34:40 +0800

    add ab test

--- loadtest/ApacheBench/jpg2json.py
+++ loadtest/ApacheBench/jpg2json.py
@@ -5,7 +5,10 @@ import json
 f = open('./dog.jpg', 'rb')
 jpg = f.read()
 image_databytes = base64.b64encode(jpg)
-post_data = {"image": image_databytes.decode('utf-8')}
+params = [{'id': '0', 'type': 'jpeg', 'data': image_databytes.decode('utf-8')}]
+n_obj = 4
+params = n_obj * params
+post_data = {'client_id': 'xxx', 'params': params}
 with open("dog_req.json", 'w', encoding='utf-8') as json_file:
     json.dump(post_data, json_file, ensure_ascii=False)
 
